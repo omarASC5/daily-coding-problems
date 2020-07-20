c36946ff6fc180e1377bd10a3afc981d5a188e05
omarASC5 <omarlcobas@gmail.com>
2020-07-19 21:16:27 -0400

    day7 complete.

--- day7.py
+++ day7.py
@@ -16,13 +16,40 @@ You can assume that the messages are decodable. For example, '001' is not allowe
 '''
 
 char_map = {
-	'a': 1, 'b': 2, 'c': 3,
-	'd': 4, 'e': 5, 'f': 6,
-	'g': 7, 'h': 8, 'i': 9,
-	'j': 10, 'k': 11, 'l': 12,
-	'm': 13, 'n': 14, 'o': 15,
-	'p': 16, 'q': 17, 'r': 18,
-	's': 19, 't': 20, 'u': 21,
-	'v': 22, 'w': 23, 'x': 24,
-	'y': 25, 'z': 26
+	'1': 'a', '2': 'b', '3': 'c',
+	'4': 'd', '5': 'e', '6': 'f',
+	'7': 'g', '8': 'h', '9': 'i',
+	'10': 'j', '11': 'k', '12': 'l',
+	'13': 'm', '14': 'n', '15': 'o',
+	'16': 'p', '17': 'q', '18': 'r',
+	'19': 's', '20': 't', '21': 'u',
+	'22': 'v', '23': 'w', '24': 'x',
+	'25': 'y', '26': 'z'
 }
+
+def helper(data, k, memo):
+	if k == 0:
+		return 1
+	s = len(data) - k
+	if data[s] == '0':
+		return 0
+	if memo[k] != None:
+		# Check if the current function call was already memoized
+		return memo[k]
+
+	# First, consider one numner at a time
+	result = helper(data, k - 1, memo)
+	# Then, consider a two digit number
+	if k >= 2 and int(data[s:s + 2]) <= 26:
+		result += helper(data, k - 2, memo)
+
+	# Memoize the function result
+	memo[k] = result
+	return result
+
+def num_ways(data):
+	# Make a memo list to memoize previous function calls
+	memo = [None] * (len(data) + 1)
+	return helper(data, len(data), memo)
+
+print(num_ways('111111'))
